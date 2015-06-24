4196f966d9195fed17662b1f825372dcb32b74c0
Alex Shepard <alex@meatfreezer.com>
2015-06-24 12:55:39 -0700

    analysis of algorithms

---
+++ Analysis of Algorithms.playground/Contents.swift
@@ -0,0 +1,81 @@
+//: ## Week 1 - Analysis of Algorithms
+
+import CoreFoundation
+
+//: ### ThreeSum Protocol
+
+protocol ThreeSum {
+    static func count([Int]) -> Int
+}
+
+//: ### Test Data
+
+//: ```makeList()``` - 
+//: Helper function to make a list of random ints below 100
+//: 
+//: _this isn't terribly efficient, could re-use previously generated ints_
+
+func makeList(count: Int) -> [Int] {
+    var list = [Int]()
+    for _ in 0..<count {
+        list.append(Int(arc4random()) % 100)
+    }
+    return list
+}
+
+//: ```lists``` -
+//: list of 10 lists of integers, doubling in size. 
+//: 
+//: _in the bruteforce implementation, this should produce a logRatio of 
+//: running times that converges to 3, based on the efficiency (or lack 
+//: thereof) of the bruteforce algorithm._
+
+let lists: [[Int]] = {
+    var lists = [[Int]]()
+    for i in 1..<10 {
+        let m = Int(pow(2.0, Double(i)))
+        lists.append(makeList(m))
+    }
+    return lists
+}()
+
+//: ### Brute Force ThreeSum implementation
+
+struct BruteForce: ThreeSum {
+    static func count(a: [Int]) -> Int {
+        var count = 0
+        for i in 0..<a.count {
+            for j in i+1..<a.count {
+                for k in j+1..<a.count {
+                    if (a[i]+a[j]+a[k] == 0) {
+                        count++
+                    }
+                }
+            }
+        }
+        return count
+    }
+}
+
+//: ### Measure Brute Force ThreeSum implementation
+
+//: _stash previous elapsed time so we can calculate the ratio of the current
+//: elapsed time to the previous elapsed time._
+var prevElapsed = 0.0
+
+for list in lists {
+    let start = CFAbsoluteTimeGetCurrent()
+    BruteForce.count(list)
+    // convert elapsed to millis
+    let elapsed = (CFAbsoluteTimeGetCurrent() - start) * 1000
+    if (prevElapsed > 0) {
+        let ratio = elapsed / prevElapsed
+        // doubling input size, should converge to 3
+        let logRatio = log2(ratio)
+    }
+    prevElapsed = elapsed
+}
+
+
+
+
