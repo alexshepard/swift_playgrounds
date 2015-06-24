cf2396f18edbba0aec25a2a6d5bf44fcf9f3df8c
Alex Shepard <alex@meatfreezer.com>
2015-06-23 23:32:33 -0700

    quick-union

--- union-find.playground/Contents.swift
+++ union-find.playground/Contents.swift
@@ -64,5 +64,66 @@ qf.union(5, q: 0)
 qf.union(7, q: 2)
 qf.union(6, q: 1)
 
+//: Quick-union (lazy approach)
+class QuickUnion1: UnionFind, CustomStringConvertible {
+    var id: [Int]
+    
+    required init(n: Int) {
+        id = [Int]()
+        for i in 0..<n {
+            id.append(i)
+        }
+    }
+    
+    // helper to find the root of a node
+    func root(p:Int) -> Int {
+        var i = p
+        while i != id[i] {
+            i = id[i]
+        }
+        return i
+        
+        /* or recursively
+        if id[p] == p {
+            return p
+        } else {
+            return root(id[p])
+        }
+        */
+    }
+    
+    func union(p: Int, q: Int) {
+        id[root(p)] = id[root(q)]
+    }
+    
+    func connected(p: Int, q: Int) -> Bool {
+        return root(id[p]) == root(id[q])
+    }
+    
+    func find(p: Int) -> Int {
+        return id[p]
+    }
+    
+    func count() -> Int {
+        return id.count
+    }
+    
+    // customStringConvertible
+    var description: String {
+        return "QuickUnion1: \(self.id)"
+    }
+}
 
-
+//: Quick-union (lazy) demo
+var qu = QuickUnion1(n: 10)
+qu.union(4, q: 3)
+qu.union(3, q: 8)
+qu.union(6, q: 5)
+qu.union(9, q: 4)
+qu.union(2, q: 1)
+qu.connected(8, q: 9)
+qu.connected(5, q: 0)
+qu.union(5, q: 0)
+qu.union(7, q: 2)
+qu.union(6, q: 1)
+qu.union(7, q: 3)
